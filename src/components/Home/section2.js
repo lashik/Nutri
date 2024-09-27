b20989ac7d0d2802471c7e97716c599481915289
Vikas Talreja <vikazztalreja@gmail.com>
2024-09-27 20:54:55 +0530

    Carousel Update

--- src/components/Home/section2.js
+++ src/components/Home/section2.js
@@ -1,34 +1,43 @@
-import React from 'react';
-
+import { useState, useEffect } from 'react';
 import section2Styles from './section2.module.scss';
+import image1 from "../../assets/carousel1.jpg"
+import image2 from "../../assets/carousel2.jpeg"
+const Carousel = () => {
+  const images = [
+   image1,
+   image2
+  ];
 
-function renderSection2(props) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const totalImages = images.length;
+
+  const goToNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % totalImages);
+  };
+
+  const goToPrevious = () => {
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + totalImages) % totalImages);
+  };
+
+  useEffect(() => {
+    const interval = setInterval(goToNext, 5000); // 5 seconds
+    return () => clearInterval(interval); // Clear interval on component unmount
+  }, []);
+
   return (
-    <section className={section2Styles.section2}>
-      <div className={section2Styles.group}>
-        <div className={section2Styles.color8} />
-
-        <div className={section2Styles.flex_col}>
-          <img className={section2Styles.image2} src={'/assets/427b6082687c292bbd747a0a035ab18e.svg'} alt="alt text" />
-
-          <div className={section2Styles.wrapper1}>
-            <h1 className={section2Styles.wrapper11}>Discover</h1>
-          </div>
-
-          <div
-            className={section2Styles.wrapper2}
-            style={{ '--src': `url(${'/assets/7335ec2c10ea67783816fec679a25ab9.png'})` }}
-          />
-          <button className={section2Styles.btn}>
-            {/* TODO */}
-            LEARN MORE
-          </button>
-        </div>
-
-        <div className={section2Styles.color} />
+    <div className={section2Styles.carousel}>
+      <div className={section2Styles.carouselItem}>
+        <img src={images[currentIndex]} alt={`Carousel Item ${currentIndex + 1}`} />
       </div>
-    </section>
-  );
-}
 
-export default renderSection2;
+      <button className={section2Styles.carouselPrev} onClick={goToPrevious}>
+        Previous
+      </button>
+      <button className={section2Styles.carouselNext} onClick={goToNext}>
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Carousel;
