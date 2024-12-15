cc9ec6b983bea471c07ac2d08f34ab8156df1ba4
Kashik Sredharan <kashik.sredharan@gmail.com>
2024-12-15 06:06:54 +0530

    main

--- src/components/footer.js
+++ src/components/footer.js
@@ -17,7 +17,12 @@ function Footer() {
           <div>
             <p className={section5Styles.highlight16}>About the clinic</p>
             <p className={section5Styles.highlight14}>
-              <CustomLink to="/aboutme">Our story</CustomLink>
+                
+               <a href="https://mail.google.com/mail/?view=cm&fs=1&to=gayatribarve@thenutrinavigator.com&su=Subject&body=Let's%20set%20up%20a%20session" >
+               Email id: gayatribarve@thenutrinavigator.com
+                  </a>
+
+               <a href="tel:+91">Contact no:</a> 
               <CustomLink to="/aboutme">About us</CustomLink>
             </p>
           </div>
@@ -31,18 +36,18 @@ function Footer() {
             <p className={section5Styles.highlight16}>Join our network</p>
             <p className={section5Styles.highlight14}>
               <CustomLink to="/booknow">Contact us</CustomLink>
-            </p>
-            <p className={section5Styles.highlight14}>
-              <a href="https://www.instagram.com/the_nutrinavigator/">The Nutri Navigator on Instagram</a>
-            </p>
-            
+            </p> 
             <div className={section5Styles.flex_row3}>
             <a href="https://www.google.com/maps/place/Palkars+Polyclinic/@19.0245758,72.8345638,17z/data=!3m1!4b1!4m6!3m5!1s0x3be7cecf74d3536d:0xadc9971f08a548d0!8m2!3d19.0245707!4d72.8371387!16s%2Fg%2F1tjcmy27?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D"><img
                 className={section5Styles.image19}
                 src={"/assets/pin.png"}
                 alt="alt text"
               /></a>
-              
+            <a href="https://www.instagram.com/the_nutrinavigator/"><img
+                className={section5Styles.image19}
+                src={"/assets/instagram.png"}
+                alt="alt text"
+              /></a>
             </div>
           </div>
         </div>
